94e72a4eca7efc0eef17f22c19b1da9cebe6e5f9
Rubing <rubing.mao@metropolia.fi>
2017-11-27 22:38:00 +0200

    School options form list

--- src/Apply.js
+++ src/Apply.js
@@ -3,20 +3,20 @@ import React, { Component } from 'react';
 class Apply extends Component {
   render() {
 
-    // const school = [
-    //     'Alppila Upper Secondary School',
-    //     'Deutsche Schule Helsinki',
-    //     'The English School (Helsinki)',
-    //     'Helsingin normaalilyseo',
-    //     'Helsingin Suomalainen Yhteiskoulu',
-    //     'Helsingin yhteislyseo',
-    //     'International School of Helsinki',
-    //     'Jokela High School',
-    //     'Postipuu School',
-    //     'Ressu Comprehensive School',
-    //     'Ressu Upper Secondary School',
-    //     'Tikkurilan lukio'
-    // ];
+    const schools = [
+        'Alppila Upper Secondary School',
+        'Deutsche Schule Helsinki',
+        'The English School (Helsinki)',
+        'Helsingin normaalilyseo',
+        'Helsingin Suomalainen Yhteiskoulu',
+        'Helsingin yhteislyseo',
+        'International School of Helsinki',
+        'Jokela High School',
+        'Postipuu School',
+        'Ressu Comprehensive School',
+        'Ressu Upper Secondary School',
+        'Tikkurilan lukio'
+    ];
 
     return (
       <div>
@@ -29,17 +29,7 @@ class Apply extends Component {
             <label htmlFor="schoolchoice1">Preference 1</label>
             <select className="form-control" id="schoolpreference1">
               <option>School Name</option>
-              <option>Alppila Upper Secondary School</option>
-              <option>Deutsche Schule Helsinki</option>
-              <option>The English School (Helsinki)</option>
-              <option>Helsingin normaalilyseo</option>
-              <option>Helsingin Suomalainen Yhteiskoulu</option>
-              <option>International School of Helsinki</option>
-              <option>Jokela High School</option>
-              <option>Postipuu School</option>
-              <option>Ressu Comprehensive School</option>
-              <option>Ressu Upper Secondary School</option>
-              <option>Tikkurilan lukio</option>
+              {schools.map(school => <option key={school}>{school}</option>)}
             </select>
           </div>
 
@@ -48,17 +38,7 @@ class Apply extends Component {
             <label htmlFor="schoolchoice2">Preference 2</label>
             <select className="form-control" id="schoolpreference2">
               <option>School Name</option>
-              <option>Alppila Upper Secondary School</option>
-              <option>Deutsche Schule Helsinki</option>
-              <option>The English School (Helsinki)</option>
-              <option>Helsingin normaalilyseo</option>
-              <option>Helsingin Suomalainen Yhteiskoulu</option>
-              <option>International School of Helsinki</option>
-              <option>Jokela High School</option>
-              <option>Postipuu School</option>
-              <option>Ressu Comprehensive School</option>
-              <option>Ressu Upper Secondary School</option>
-              <option>Tikkurilan lukio</option>
+              {schools.map(school => <option key={school}>{school}</option>)}
             </select>
           </div>
 
@@ -66,17 +46,7 @@ class Apply extends Component {
             <label htmlFor="schoolchoice2">Preference 3</label>
             <select className="form-control" id="schoolpreference2">
               <option>School Name</option>
-              <option>Alppila Upper Secondary School</option>
-              <option>Deutsche Schule Helsinki</option>
-              <option>The English School (Helsinki)</option>
-              <option>Helsingin normaalilyseo</option>
-              <option>Helsingin Suomalainen Yhteiskoulu</option>
-              <option>International School of Helsinki</option>
-              <option>Jokela High School</option>
-              <option>Postipuu School</option>
-              <option>Ressu Comprehensive School</option>
-              <option>Ressu Upper Secondary School</option>
-              <option>Tikkurilan lukio</option>
+              {schools.map(school => <option key={school}>{school}</option>)}
             </select>
           </div>
 
